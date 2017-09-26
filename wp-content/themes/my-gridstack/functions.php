2749c40c0d58e68630f6e346ae7f0d1ac5d8d3d0
marylinesb <marylinesb@gmail.com>
2017-09-27 01:18:56 +0200

    Surcharge CSS OK

--- wp-content/themes/my-gridstack/functions.php
+++ wp-content/themes/my-gridstack/functions.php
@@ -3,7 +3,7 @@
 ** activation theme
 **/
 
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+//add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
