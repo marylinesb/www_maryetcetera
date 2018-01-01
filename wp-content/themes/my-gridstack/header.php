66a619634b46079246605d3cc96c875f7021c815
marylinesb <marylinesb@gmail.com>
2018-01-01 23:55:39 +0100

    Remove title for YOAST compatibility | titre onglet

--- wp-content/themes/my-gridstack/header.php
+++ wp-content/themes/my-gridstack/header.php
@@ -29,28 +29,7 @@ $browser = $_SERVER['HTTP_USER_AGENT']; ?>
 ================================================== */ 
 echo ( $favicon = of_get_option('of_custom_favicon') ) ?  '<link rel="shortcut icon" href="'. $favicon.'"/>' : '' ?>
 
-<title>
-<?php 
-/* Print the <title> tag based on what is being viewed
-================================================== */ 
-	global $page, $paged;
-
-	wp_title( '|', true, 'right' );
-
-	// Add the blog name.
-	bloginfo( 'name' );
-
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
-
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'ellipsis' ), max( $paged, $page ) );
-
-	?>
-</title>
+<title><?php wp_title(''); ?></title>
 
 <?php 
 /* Load Google Fonts defined in functions.php
