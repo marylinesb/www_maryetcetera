4b67466c08f7baf3a1ec7946457e78560718fe79
marylinesb <marylinesb@gmail.com>
2018-01-01 23:56:12 +0100

    surcharge et suppression titre

---
+++ wp-content/themes/my-gridstack/taxonomy-filter.php
@@ -0,0 +1,117 @@
+<?php 
+/**
+ * This file displays the blog category
+ */
+get_header(); 
+
+global $add_isotope;
+
+// Add isotope script
+$add_isotope = true;
+
+// Get current filter
+$current_filter 	    = get_query_var('filter');
+$current_filter_tax   = get_terms('filter', 'slug='.$current_filter); 
+$numposts   		      = (of_get_option('of_portfolio_posts')) ? of_get_option('of_portfolio_posts') : '10'; // Number of posts
+
+// Get Portfolio Page ID and URL      
+$portfolio_pages = get_pages(array(
+  'meta_key'    => '_wp_page_template',
+  'meta_value'  => 'template-portfolio.php'
+));
+
+// If there's not template-portfolio.php page set
+if (!$portfolio_pages) {
+  $portfolio_pages = get_pages(array(
+    'meta_key'    => '_wp_page_template',
+    'meta_value'  => 'template-portfolio-fixed.php'
+  ));
+}
+
+// Set the portfolio page ID
+foreach($portfolio_pages as $port){
+    $pageID = $port->ID;
+}
+
+/**
+ * Get variables for page options
+ */
+$tw_options = themewich_page_title_options($pageID);
+
+/* #Get Only The Terms From These Filtered Portfolios
+======================================================*/
+$term_list = array();
+
+$wp_query = new WP_Query(array(
+	'posts_per_page' => $numposts,
+    'filter'         => $current_filter 
+));
+
+// Get only terms from these posts
+while ($wp_query->have_posts()) : $wp_query->the_post();
+    $term_list = array_merge($term_list, wp_get_post_terms($post->ID, 'filter', array("fields" => "ids")));
+endwhile; 
+
+ 
+
+$term_list = array_unique($term_list); // Remove Duplicate
+if(($key = array_search($current_filter_tax[0]->term_id, $term_list)) !== false) {
+    unset($term_list[$key]); // Remove Current term
+}
+$term_list = implode(',', $term_list); 
+?> 
+
+<!-- Container Wrap -->
+<div id="postcontainer">    
+
+  <!-- Grid Area -->
+  <div class="container">
+    <div class="sixteen columns isotopecontainer" data-value="3">
+    <?php
+
+    /* #Query the Posts
+    ======================================================*/
+    $wp_query = new WP_Query(array(
+                    // Ignore Sticky Posts
+                    'ignore_sticky_posts' => 1,
+                    // Add pagination
+                    'paged'          => $paged,
+  				          // Get only the most recent
+                    'posts_per_page' => $numposts,
+                    // Only Posts From Current Filter
+                    'filter'         => $current_filter 
+                ));
+    
+    /* #Loop through posts
+    ======================================================*/
+    while ($wp_query->have_posts()) : $wp_query->the_post(); 
+    
+    /* #Get Thumbnail
+    ======================================================*/
+    get_template_part('functions/templates/standard');
+    
+    endwhile; //End Loop ?>
+
+    <div class="clear"></div>     
+    </div>
+  </div>
+  <!-- END Grid Area -->
+    
+  <!-- Pagination -->
+  <?php if (get_next_posts_link()) : ?>
+  <div class="container">
+    <div class="sixteen columns">
+        <p class="more-posts"><?php next_posts_link(__('Load More Posts', 'framework')); ?></p>
+        <div class="clear"></div>
+    </div>
+  </div>
+  <?php endif; ?>
+  <!-- END Pagination -->
+
+</div>
+<!-- End containerwrap -->
+
+<?php   
+/* #Get Footer
+======================================================*/
+get_footer(); ?>
